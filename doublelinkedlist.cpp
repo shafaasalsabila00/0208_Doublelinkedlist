f5b5ff39b63223865a07da1fef3b335c7e65ee6a
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-13 10:06:16 +0700

    pembuatan node dan pemerian value

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -15,3 +15,12 @@ struct Node
 
 //deklarasi pointer START dan pemberian nilai
 Node *START = NULL;
+
+// deklarasi prosedur addNode
+void addNode()
+{
+    //pembuatan node dan pemberian value untuk data noMhs dan name
+    Node *newNode = new Node(); //step 1 buat node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; // assign value to the data field of th
+}
