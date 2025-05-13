58cf5473b7a9d047f19f85cadcff6e889b6aabc0
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-13 10:26:48 +0700

    memberi nilai prev

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -43,5 +43,8 @@ void addNode()
         {
             START->prev = newNode; //step 4: make the first node point to the
         }
+        //memberi nilai prev = null dan start = node baru
+        newNode->prev = NULL; //step 5: make the new node point to NULL
+        START = newNode; //step 6: make the new node the first node
     }
 }
