71763d144ea368c8381e756b77fa847098e8761c
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-13 10:22:00 +0700

    cek duplikasi

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -23,4 +23,18 @@ void addNode()
     Node *newNode = new Node(); //step 1 buat node baru
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs; // assign value to the data field of th
+
+    //insert the new node in the list
+    //kondisi jika star == null atau noMhs node baru <= noMhs start
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        //step 2: insert the new node at the beginning
+        // kondisi jika star tidak kosong dan noMhs node baru sama dengan noMhs
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+        
+    }
 }
