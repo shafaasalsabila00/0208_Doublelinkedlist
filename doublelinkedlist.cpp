15e39931b67d32cf48700408b4865585b860a651
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-13 11:11:48 +0700

    mencari node yang akan dihapus

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -115,5 +115,21 @@ void deleteNode()
     }
 
     current = START; // step 1: start from the first node
-    previous = NULL;
+    previous = NULL; 
+
+    //locate the node to be deleted
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number "
+        << rollNo << " not found\033[0m" << endl;
+        return;
+    }
+
+    
 }
